1ecd9932feb06155261e7b1928bdce614b162556
hopoduck <d>
2020-06-19 08:58:17 +0900

    bg issue fix

--- src/background.js
+++ src/background.js
@@ -1,31 +1,29 @@
 const body = document.querySelector("body");
 const img = document.createElement("img");
 
+img.classList.add("bg");
+
 window.addEventListener("resize", resizeHandler);
 
 function resizeHandler() {
-  if (window.innerWidth >= 1920) {
-    img.style.width = "100%";
-    img.style.height = "auto";
-  } else {
-    img.style.width = "auto";
-    img.style.height = "100%";
-  }
+  // if (window.innerWidth >= 1920) {
+  //   img.style.width = "100%";
+  //   img.style.height = "auto";
+  // } else {
+  //   img.style.width = "auto";
+  //   img.style.height = "100%";
+  // }
+  img.style.width = "100%";
+  img.style.height = "100%";
 }
 
 function init() {
   const random = parseInt(Math.random() * 4) + 1;
   img.src = `src/bg_img/${random}.jpg`;
-  if (window.innerWidth >= 1920) {
-    img.style.width = "100%";
-    img.style.height = "auto";
-  } else {
-    img.style.width = "auto";
-    img.style.height = "100%";
-  }
+    resizeHandler();
   img.style.position = "absolute";
   img.style.top = "0px";
-  img.style.right = "0px";
+  img.style.left = img.width / 2;
   img.style.zIndex = -50;
   body.appendChild(img);
 }
